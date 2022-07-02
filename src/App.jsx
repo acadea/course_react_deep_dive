401dc286616f5a8e569210f45b7130d7e3f45c3a
Sam <opensource@acadea.io>
2022-07-02 19:52:32 +0800

    added react state

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,22 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // let abc = "123";
+  const [abc, setAbc] = useState("123");
+
+  setTimeout(() => {
+    // console.log('settimeout')
+    // abc = "456";
+    setAbc("456");
+  }, 2000);
+
+  const paragraph = <p>{abc}</p>
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + AAAA React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code>  daiojdoiw and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>{abc}</h1>
+
+      {paragraph}
     </div>
   )
 }
