d3eaa8466e216b406acbd2019ab38756de674391
Sam <opensource@acadea.io>
2022-08-28 08:18:50 +0800

    added navbar with link component

--- src/layouts/public/navbar/Navbar.jsx
+++ src/layouts/public/navbar/Navbar.jsx
@@ -1,62 +1,32 @@
 import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import css from "./Navbar.module.css";
 
 export default function Navbar() {
 
-  console.log(css);
-
   const navItems = [
     {
       title: 'Home',
-      href: '#',
+      to: '/',
     },
     {
       title: 'Contact',
-      href: '#',
+      to: '/contact',
     },
-    {
-      title: 'About',
-      href: '#',
-    },
-    {
-      title: 'About me',
-      href: '#',
-    }
+ 
 
   ];
 
   function renderNavItems() {
     return navItems.map((navItem) => (
       <li key={navItem.title}>
-        <a href={navItem.href}>{navItem.title}</a>
+        <Link to={navItem.to} > {navItem.title} </Link>
+        {/* <a href={navItem.href}>{navItem.title}</a> */}
       </li>
     ))
   }
-
-
-  const [counterA, setCounterA] = useState(0);
-  const [counterB, setCounterB] = useState(0);
-
-  // listen to all state changes
-  useEffect(() => {
-    // logic
-    console.log('listen to all');
-  })
-
-  // not listening to any state change
-  // will trigger only once when the component was mounted
-  useEffect(() => {
-    // logic
-    console.log('once onlyyy')
-  }, [])
-
-  // listen to state passed in only
-  useEffect(() => {
-    // logic
-    console.log('counter A is changing')
-  }, [counterA]);
 
   return (
     <nav className={css.nav}>
