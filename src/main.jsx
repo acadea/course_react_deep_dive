51434cd744b9b5864db3a3dcca3dce90256fbd61
Sam <opensource@acadea.io>
2022-09-16 23:11:01 +0800

    refactored route registration to data api

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import 'virtual:windi.css';
 import './index.css'
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, HashRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Dashboard from './pages/admin/Dashboard';
+import SettingIndex from './pages/admin/settings/SettingIndex';
+import SettingGeneral from './pages/admin/settings/SettingGeneral';
+import SettingSite from './pages/admin/settings/SettingSite';
+import SettingTheme from './pages/admin/settings/SettingTheme';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/contact',
+    element: <Contact />
+  },
+  {
+    path: '/admin',
+    children: [
+      { path: 'dashboard', element: <Dashboard /> },
+      { path: 'manage', element: <Dashboard /> },
+      { path: 'manage/user/:userId', element: <Dashboard /> },
+      {
+        path: 'settings',
+        element: <SettingIndex />,
+        children: [
+          { path: 'general', element: <SettingGeneral /> },
+          { path: 'site', element: <SettingSite /> },
+          { path: 'theme', element: <SettingTheme /> },
+        ]
+      },
+    ]
+  }
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    
-      <App />
-    
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
